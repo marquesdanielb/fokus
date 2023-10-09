fcc2e578985e805dad56ab09922e6451e973674a
Daniel Marques Barbosa <marquesdanielb@gmail.com>
2023-10-09 14:38:11 -0300

    Add behavior to change img in respective data-context

--- script.js
+++ script.js
@@ -3,15 +3,20 @@ const html = document.querySelector('html');
 const focusBt = document.querySelector('.app__card-button--focus');
 const shortBt = document.querySelector('.app__card-button--short');
 const longBt = document.querySelector('.app__card-button--long');
+const banner = document.querySelector('.app__image');
+
 
 focusBt.addEventListener('click', () => {
     html.setAttribute('data-context', 'focus');
+    banner.setAttribute('src' ,'./img/focus.png');
 });
 
 shortBt.addEventListener('click', () => {
     html.setAttribute('data-context', 'short');
+    banner.setAttribute('src' , './img/short-break.png');
 });
 
 longBt.addEventListener('click', () => {
     html.setAttribute('data-context', 'long');
+    banner.setAttribute('src' , './img/long-break.png');
 });
